026cbc44768d29f28b0277309a967b98cb256859
v-rapha <vs.raphael19@gmail.com>
2020-09-11 22:09:42 -0300

    Jest configuration & first test

---
+++ test/functional/works.test.ts
@@ -0,0 +1,23 @@
+import supertest from 'superagent';
+
+describe('Works functional tests', () => {
+  it('should return a list with just a few works', async () => {
+    const { body, status } = await supertest(app).get('/works');
+    expect(status).toBe(200);
+    expect(body).toEqual([{
+      "client": "José Will",
+      "description": "KSF90-Engrenagem",
+      "price": 190.90,
+      "initialDate": "11/09/2020",
+      "finalDate": "15/09/2020",
+      "timeLeft": 4
+    },{
+      "client": "Will Smith",
+      "description": "KSF50-Engrenagem",
+      "price": 120.55,
+      "initialDate": "09/09/2020",
+      "finalDate": "08/09/2020",
+      "timeLeft": 1
+    }]);
+  });
+})
