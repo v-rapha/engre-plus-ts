dc6c04dd877905a4706cee624c5b01097638069e
v-rapha <vs.raphael19@gmail.com>
2020-11-20 01:17:13 -0300

    Production env var

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import config from 'config';
 
 (async (): Promise<void> => {
   console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
+  console.log('NODE_CONFIG_DIR: ' + config.util.getEnv('NODE_CONFIG_DIR'));
   const server = new SetupServer(config.get('App.port'));
   await server.init();
   server.start();
