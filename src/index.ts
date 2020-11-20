bd57aa1c418c78dac2491572882c1ce085d7d521
v-rapha <vs.raphael19@gmail.com>
2020-11-20 01:05:48 -0300

    Production env var & test var for start:dev script

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { SetupServer } from './server';
 import config from 'config';
 
 (async (): Promise<void> => {
+  console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
   const server = new SetupServer(config.get('App.port'));
   await server.init();
   server.start();
