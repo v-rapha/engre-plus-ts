94ce64e84eef3656985cee8fbb77cb97254a4f1a
v-rapha <vs.raphael19@gmail.com>
2020-11-21 00:06:03 -0300

    Change config file name for 'production'

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -13,6 +13,8 @@ const migrationsPath: string = config.has('App.typeorm_path.migrations')
 console.log(entitiesPath);
 console.log(migrationsPath);
 console.log('POSTGRESQL: ' + config.get('App.database.postgresUrl'))
+console.log('PORT: ' + config.get('App.port'))
+console.log('KEY: ' + config.get('App.auth.key'))
 
 const connectionManager = getConnectionManager();
 export const connection = connectionManager.create({
